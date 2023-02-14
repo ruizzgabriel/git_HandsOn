94964249d0ef5ab501ef568f06624a82787cf920
Sergi Soldevila <sergi.soldevila@uvic.cat>
2023-02-14 17:11:11 +0100

    Revert "percentage"
    This reverts commit 1187a4fa959f3c93034201ecdd414212f04a911c.

--- seqClass.py
+++ seqClass.py
@@ -23,19 +23,19 @@ args.seq = args.seq.upper()
 # Check if the sequence is DNA or RNA
 # First condition: assess that the sequence contains nucleotides
 if re.search("^[ACGTU]+$", args.seq):
-    # If the sequence has "T" and it does not contain "U", it is DNA
+    # If the sequence has "T" and it do not contains "U", is DNA
     if re.search("^[^U]+$", args.seq) and re.search("T", args.seq):
         print("The sequence is DNA")
-    # If the sequence has "U" and no "T", it is RNA
+    # If the sequence has "U" and no "T", is RNA
     elif re.search("^[^T]+$", args.seq) and re.search("U", args.seq):
         print("The sequence is RNA")
-    # If the sequence does not contain "T" nor "U", the sequence can be DNA or RNA
+    # If the sequence do not contains "T" nor "U", the sequence can be DNA or RNA
     elif re.search("^[^T]+$", args.seq) and re.search("^[^U]+$", args.seq):
         print("The sequence can be DNA or RNA")
     # If the sequence is not following any of the previous conditions, the sequence is a DNA/RNA mix
     else:
         print("You introduced a RNA/DNA mix, that is not possible.")
-# If the sequence contains a letter that does not correspond to nucleotide code, it is not DNA nor RNA.
+# If the sequence contains a letter that do not correspond to nucleotide code, it is not DNA nor RNA.
 else:
     print("The sequence is not DNA nor RNA")
 
@@ -51,26 +51,3 @@ if args.motif:
         print("It is found")
     else:
         print("It is not found")
-
-# Count the number of each nucleotide in the sequence
-nucleotide_counts = {
-    "A": args.seq.count("A"),
-    "C": args.seq.count("C"),
-    "G": args.seq.count("G"),
-    "T": args.seq.count("T"),
-    "U": args.seq.count("U")
-}
-
-# Calculate the total length of the sequence
-seq_length = len(args.seq)
-
-# Calculate the percentage of each nucleotide in the sequence
-nucleotide_percentages = {
-    k: (v / seq_length) * 100 for k, v in nucleotide_counts.items()
-}
-
-# Print the percentage of each nucleotide
-print("Percentage of each nucleotide:")
-for k, v in nucleotide_percentages.items():
-    print(f"{k}: {v:.2f}%")
-
